696e9e247d9b9725d3944dd1439d8b6d9c324c81
SandaliW <49431773+SandaliW@users.noreply.github.com>
2019-10-21 12:23:23 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 # app.py
 
 from flask import Flask
-#from flask import jsonif,request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-#########@app.route('/', methods=['GET'])
 class Student(Resource):
     def get(self, Sandali):
-        return ('student',Sandali)
+        return {'student': 'Sandali'}
 
 api.add_resource(Student, '/student/<string:Sandali>')
+#http://127.0.0.1:5000/student/Rolf
 
 app.run(port=5000)
