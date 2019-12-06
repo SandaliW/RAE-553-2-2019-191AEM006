04b7b8c57f26d43b7c5dab320e60340c6657a920
SandaliW <49431773+SandaliW@users.noreply.github.com>
2019-12-06 21:28:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from flask import Flask
 from flask_restful import Resource, Api
 from flask_jwt_extended import jwt_required 
 from security import authenticate
+# i always get an error when i execute with these lines. however i try i can't seem to find the correct answer. 
+#from flask_jwt_extended import JWT, jwt_required and from security import authenticate, idenetity
+# but without JWT and identity code will run but then again i am geeting 404 at postman
 
 app = Flask(__name__)
 app.secret_key = 'john'
